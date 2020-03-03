c4448ea0ec635293c75880b73c5af2973dad6035
Huw Strafford <h.a.strafford@swansea.ac.uk>
2020-03-03 10:41:00 +0000

    Groovy start
    For Seizure Frequency need to add string into the output string = :match@string

---
+++ Groovy/Diagnosis.groovy
@@ -0,0 +1,9 @@
+new File(scriptParams.outputFile).withWriterAppend{ out ->
+  doc.getAnnotations("Epilepsy").get("Diagnosis").each{
+    anno ->
+      def f = anno.getFeatures()
+      def (A,B,C) =  doc.getFeatures().get("gate.SourceURL").split("_|\\.")
+      String[] id = A.split("/")
+      out.writeLine(/"${id[-1]}","${f.get('CUI')}","${f.get('Negation')}","${f.get('Certainty')}","${f.get('rule')}",/)
+    }
+  }
