09647ec3eb4faa8d4019e6670217b2dc1f80baf0
Huw Strafford <h.a.strafford@swansea.ac.uk>
2020-06-25 10:51:10 +0100

    Updates for Sam Machine Learning Prescription

--- Groovy/PrescriptionJson.groovy
+++ Groovy/PrescriptionJson.groovy
@@ -5,6 +5,6 @@ new File(scriptParams.outputFile).withWriterAppend{ out ->
       def f = anno.getFeatures()
       def (A,B,C) =  doc.getFeatures().get("gate.SourceURL").split("_|\\.")
       String[] id = C.split("/")
-      out.writeLine(/"${id[-1]}","${f.get('String1')}","CUI","${f.get('CUI')}","DrugDose","${f.get('DrugDose')}","DoseUnit","${f.get('DoseUnit')}","Frequency","${f.get('Frequency')}",/)
+      out.writeLine(/"${id[-1]}","${f.get('String1')}","CUI","${f.get('CUI')}","DrugDose","${f.get('DrugDose')}","DoseUnit","${f.get('DoseUnit')}","Frequency","${f.get('Frequency')}","isPrescription","${f.get('isPrescription')}",/)
   }
 }
